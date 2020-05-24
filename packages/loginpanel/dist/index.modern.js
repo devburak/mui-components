37ceeaab4d4df0393a3fb97d3907ed712b56c22d
devburak <dev.burak@gmail.com>
2020-05-24 22:04:20 +0200

    .

--- packages/loginpanel/dist/index.modern.js
+++ packages/loginpanel/dist/index.modern.js
@@ -18,10 +18,26 @@ import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import IconButton from '@material-ui/core/IconButton';
 
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
 function Copyright(props) {
-  const {
-    website
-  } = props;
+  var website = props.website;
   return /*#__PURE__*/React.createElement(Typography, {
     variant: "body2",
     color: "textSecondary",
@@ -32,108 +48,121 @@ function Copyright(props) {
   }, website.name), ' ', new Date().getFullYear(), '.');
 }
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  },
-  large: {
-    width: theme.spacing(7),
-    height: theme.spacing(7)
-  }
-}));
+var useStyles = makeStyles(function (theme) {
+  return {
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main
+    },
+    form: {
+      width: '100%',
+      marginTop: theme.spacing(1)
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2)
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7)
+    }
+  };
+});
 
 function LoginPanel(props) {
-  const [showPassword, setShowPass] = useState(false);
-  const classes = useStyles();
-  const {
-    website = {
-      name: 'website',
-      url: 'https://material-ui.com',
-      copyrightvisible: true,
-      logo: '/broken-image.jpg'
-    },
-    formControl = {
-      label: 'Remember me',
-      control: {
-        value: 'remember',
-        color: 'primary'
-      }
-    },
-    forgetpassword = {
-      text: 'Forgot password?',
-      link: '#',
-      visible: true
-    },
-    signin = {
-      text: 'SIGN IN',
-      icon: undefined
-    },
-    signup = {
-      text: "Don't have an account? Sign Up",
-      link: '#',
-      visible: true
-    },
-    onSubmit,
-    isbusy = false,
-    progressprops = {
-      color: 'primary',
-      size: 21
-    },
-    buttonprops = {
-      color: 'primary',
-      type: 'submit',
-      variant: 'contained'
-    },
-    usernameprops = {
-      InputProps: {
-        startAdornment: /*#__PURE__*/React.createElement(InputAdornment, {
-          position: "start"
-        }, /*#__PURE__*/React.createElement(AccountCircle, null))
-      },
-      label: 'User Name or Email',
-      fullWidth: true,
-      name: 'username',
-      autoComplete: 'email',
-      autoFocus: true,
-      variant: 'outlined',
-      margin: 'normal',
-      required: true
-    },
-    passwordprops = {
-      InputProps: {
-        startAdornment: /*#__PURE__*/React.createElement(InputAdornment, {
-          position: "start"
-        }, /*#__PURE__*/React.createElement(LockRounded, null))
-      },
-      autoComplete: 'current-password',
-      variant: 'outlined',
-      margin: 'normal',
-      required: true,
-      fullWidth: true,
-      name: 'password',
-      label: 'Password'
+  var _useState = useState(false),
+      showPassword = _useState[0],
+      setShowPass = _useState[1];
+
+  var classes = useStyles();
+  var _props$website = props.website,
+      website = _props$website === void 0 ? {
+    name: 'website',
+    url: 'https://material-ui.com',
+    copyrightvisible: true,
+    logo: '/broken-image.jpg'
+  } : _props$website,
+      _props$formControl = props.formControl,
+      formControl = _props$formControl === void 0 ? {
+    label: 'Remember me',
+    control: {
+      value: 'remember',
+      color: 'primary'
     }
-  } = props;
+  } : _props$formControl,
+      _props$forgetpassword = props.forgetpassword,
+      forgetpassword = _props$forgetpassword === void 0 ? {
+    text: 'Forgot password?',
+    link: '#',
+    visible: true
+  } : _props$forgetpassword,
+      _props$signin = props.signin,
+      signin = _props$signin === void 0 ? {
+    text: 'SIGN IN',
+    icon: undefined
+  } : _props$signin,
+      _props$signup = props.signup,
+      signup = _props$signup === void 0 ? {
+    text: "Don't have an account? Sign Up",
+    link: '#',
+    visible: true
+  } : _props$signup,
+      onSubmit = props.onSubmit,
+      _props$isbusy = props.isbusy,
+      isbusy = _props$isbusy === void 0 ? false : _props$isbusy,
+      _props$progressprops = props.progressprops,
+      progressprops = _props$progressprops === void 0 ? {
+    color: 'primary',
+    size: 21
+  } : _props$progressprops,
+      _props$buttonprops = props.buttonprops,
+      buttonprops = _props$buttonprops === void 0 ? {
+    color: 'primary',
+    type: 'submit',
+    variant: 'contained'
+  } : _props$buttonprops,
+      _props$usernameprops = props.usernameprops,
+      usernameprops = _props$usernameprops === void 0 ? {
+    InputProps: {
+      startAdornment: /*#__PURE__*/React.createElement(InputAdornment, {
+        position: "start"
+      }, /*#__PURE__*/React.createElement(AccountCircle, null))
+    },
+    label: 'User Name or Email',
+    fullWidth: true,
+    name: 'username',
+    autoComplete: 'email',
+    autoFocus: true,
+    variant: 'outlined',
+    margin: 'normal',
+    required: true
+  } : _props$usernameprops,
+      _props$passwordprops = props.passwordprops,
+      passwordprops = _props$passwordprops === void 0 ? {
+    InputProps: {
+      startAdornment: /*#__PURE__*/React.createElement(InputAdornment, {
+        position: "start"
+      }, /*#__PURE__*/React.createElement(LockRounded, null))
+    },
+    autoComplete: 'current-password',
+    variant: 'outlined',
+    margin: 'normal',
+    required: true,
+    fullWidth: true,
+    name: 'password',
+    label: 'Password'
+  } : _props$passwordprops;
   return /*#__PURE__*/React.createElement(Container, {
     component: "main",
     maxWidth: "xs"
   }, /*#__PURE__*/React.createElement(CssBaseline, null), /*#__PURE__*/React.createElement("div", {
     className: classes.paper
-  }, /*#__PURE__*/React.createElement(Avatar, Object.assign({
+  }, /*#__PURE__*/React.createElement(Avatar, _extends({
     alt: website.name,
     src: website.logo,
     className: classes.large
@@ -143,19 +172,21 @@ function LoginPanel(props) {
   }, website.name), /*#__PURE__*/React.createElement("form", {
     className: classes.form,
     onSubmit: onSubmit
-  }, /*#__PURE__*/React.createElement(TextField, Object.assign({
+  }, /*#__PURE__*/React.createElement(TextField, _extends({
     id: "username"
-  }, usernameprops)), /*#__PURE__*/React.createElement(TextField, Object.assign({
+  }, usernameprops)), /*#__PURE__*/React.createElement(TextField, _extends({
     type: showPassword ? 'text' : 'password',
     id: "password"
   }, passwordprops, {
-    InputProps: { ...passwordprops.InputProps,
+    InputProps: _extends(_extends({}, passwordprops.InputProps), {}, {
       endAdornment: /*#__PURE__*/React.createElement(IconButton, {
         "aria-label": "toggle password visibility",
-        onClick: () => setShowPass(!showPassword),
+        onClick: function onClick() {
+          return setShowPass(!showPassword);
+        },
         edge: "end"
       }, showPassword ? /*#__PURE__*/React.createElement(Visibility, null) : /*#__PURE__*/React.createElement(VisibilityOff, null))
-    }
+    })
   })), /*#__PURE__*/React.createElement(FormControlLabel, {
     control: /*#__PURE__*/React.createElement(Checkbox, {
       id: "rememberme",
@@ -163,7 +194,7 @@ function LoginPanel(props) {
       color: formControl.control.color
     }),
     label: formControl.label
-  }), /*#__PURE__*/React.createElement(LoadingButton, Object.assign({
+  }), /*#__PURE__*/React.createElement(LoadingButton, _extends({
     type: "submit",
     fullWidth: true,
     className: classes.submit,
